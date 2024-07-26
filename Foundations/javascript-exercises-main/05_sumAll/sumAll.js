828b0bd2770b24bd39cdc3245310ecf1d2f678d4
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-07-26 01:53:34 -0400

    Completed javascript exercise 05.

--- Foundations/javascript-exercises-main/05_sumAll/sumAll.js
+++ Foundations/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
 
+    // Return ERROR if two valid numbers are not arguments.
+    if (
+        (Number.isInteger(firstNumber) === false || Number.isInteger(secondNumber) === false) ||
+        (firstNumber < 0 || secondNumber < 0) ||
+        (firstNumber % 1 != 0 || secondNumber % 1 != 0)
+    ) {
+        return "ERROR";
+    }
+
+
+    // Return the sum of the range of arguments.
+
+    let smallerNumber;
+    let biggerNumber;
+    let sum = 0;
+
+    if (firstNumber < secondNumber) {
+        smallerNumber = firstNumber;
+        biggerNumber = secondNumber;
+    } else if (firstNumber > secondNumber) {
+        biggerNumber = firstNumber;
+        smallerNumber = secondNumber;
+    }
+
+    for (let i = smallerNumber; i <= biggerNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
