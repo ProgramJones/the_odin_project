a20cffe3293cf7da6557e8d4f05a38788fa5f309
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-07-26 01:12:00 -0400

    Completed the javascript exercises 04 exercise.

--- Foundations/javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ Foundations/javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+
+    let duplicateItemIndex;
+
+    for (let item of args) {
+        while (array.includes(item)) {
+            duplicateItemIndex = array.indexOf(item);
+            array.splice(duplicateItemIndex, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
