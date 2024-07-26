dbfa06bb9ac9471402893a28ec12314177aa4914
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-07-26 00:04:17 -0400

    Completed all tests for javascript 04 exercise.

--- Foundations/javascript-exercises-main/03_reverseString/reverseString.js
+++ Foundations/javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (initialString) {
+    let initialStringArray = initialString.split("");
+    let reversedStringArray = [];
 
+    for (let letter of initialStringArray) {
+        reversedStringArray.unshift(letter);
+    }
+
+    let reversedString = reversedStringArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
