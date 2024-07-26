d9a82d668daabcadeaab87b835e6b066a6a45909
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-07-25 23:56:40 -0400

    Completed all tests for the 02 exercise.

--- Foundations/javascript-exercises-main/02_repeatString/repeatString.js
+++ Foundations/javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,4 +1,23 @@
-const repeatString = function() {
+const repeatString = function (initialString, number) {
+
+    if (number < 0) {
+        return "ERROR";
+    } else if (number === 0) {
+        return "";
+    } else if (number === 1) {
+        return initialString;
+    } else if (number > 1) {
+        let finalString = "";
+
+        for (i = 0; i < number; i++) {
+            finalString += initialString;
+        }
+
+        return finalString;
+    } else if (initialString === "") {
+        return "";
+    }
+
 
 };
 
