6aa25c88113555ed7fde82e77ff74e433bc9aac3
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-07-26 02:19:49 -0400

    Completed javascript 07 exercise.

--- Foundations/javascript-exercises-main/07_tempConversion/tempConversion.js
+++ Foundations/javascript-exercises-main/07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+    let conversion = (temp * (9 / 5)) + 32;
+    conversion = Math.round(conversion * 10) / 10 // Round to one decimal place
+
+    return conversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+    let conversion = (temp - 32) * (5 / 9);
+    conversion = Math.round(conversion * 10) / 10 // Round to one decimal place
+
+    return conversion;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
