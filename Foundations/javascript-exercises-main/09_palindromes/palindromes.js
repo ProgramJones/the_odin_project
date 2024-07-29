1bb07570497949d6f0bdb8cdf17d6ab4107213cf
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-07-29 18:30:00 -0400

    Completed the javascript 09 exercise.

--- Foundations/javascript-exercises-main/09_palindromes/palindromes.js
+++ Foundations/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let modifiedStr = str.toLowerCase();
+
+    // Removes the last character if it's a special character
+    if (/[.?!]$/.test(modifiedStr)) {
+        modifiedStr = modifiedStr.slice(0, -1);
+    }
+
+    // Removes any spaces and commas from str
+    let tempArray = modifiedStr.split("");
+    tempArray = tempArray.filter((item) => /\S/.test(item) && /[^,]/.test(item));
+    modifiedStr = tempArray.join("");
+
+    let reversedArray = modifiedStr.split("").reverse().join("");
+
+    return (modifiedStr == reversedArray) ? true : false;
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
