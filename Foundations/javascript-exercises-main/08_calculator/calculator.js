4865f14abdef17325d80afa4441e2987f95aa0f8
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-07-29 00:21:38 -0400

    Completed the javascript 08 exercise.

--- Foundations/javascript-exercises-main/08_calculator/calculator.js
+++ Foundations/javascript-exercises-main/08_calculator/calculator.js
@@ -1,33 +1,67 @@
-const add = function() {
-	
+const add = function (firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+
+    if (arr.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+
+        sum = arr.reduce((partialSum, item) => {
+            return partialSum + item;
+        });
+
+        return sum;
+    }
+
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+    result = arr.reduce((accumulator, item) => {
+        return accumulator *= item;
+    }, 1)
 
+    return result;
 };
 
-const power = function() {
-	
+const power = function (firstNumber, secondNumber) {
+    result = 0;
+
+    for (let i = 0; i <= secondNumber; i++) {
+        result += firstNumber * firstNumber;
+    }
+
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+
+    if (number == 0 || number == 1) {
+        return 1;
+    } else {
+        let total = 1;
+
+        for (let i = 1; i <= number; i++) {
+            total *= i;
+        }
+
+        return total;
+    }
+
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
