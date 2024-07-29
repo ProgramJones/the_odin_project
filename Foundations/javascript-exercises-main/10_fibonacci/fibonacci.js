3cb6f2a69f93552d19f05c57f775ef16c34a5a3a
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-07-29 19:23:43 -0400

    Completed the javascript 10 exercise.

--- Foundations/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ Foundations/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
 
+    if (index < 0) {
+        return "OOPS";
+    } else {
+        fibonacciSequence = [0, 1];
+
+        let total = 0;
+
+        while (fibonacciSequence[+index] === undefined) {
+            fibonacciSequence.push(fibonacciSequence.at(-1) + fibonacciSequence.at(-2));
+        }
+
+        return fibonacciSequence[+index];
+    }
 };
 
 // Do not edit below this line
