d0db22fac2a40184761d03a73e9a9cfdde7e3a7d
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-07-29 20:06:21 -0400

    Finished the javascript 12 exercise.

--- Foundations/javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ Foundations/javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrayOfPeopleObjects) {
+    let oldestAge = 0;
+    let oldestPersonObject = null;
 
+    for (let i = 0; i < arrayOfPeopleObjects.length; i++) {
+        let currentPersonAge = null;
+
+        if (arrayOfPeopleObjects[i].yearOfDeath === undefined) {
+            let currentYear = new Date().getFullYear();
+
+            currentPersonAge =
+                currentYear -
+                arrayOfPeopleObjects[i].yearOfBirth;
+        } else {
+            currentPersonAge =
+                arrayOfPeopleObjects[i].yearOfDeath -
+                arrayOfPeopleObjects[i].yearOfBirth;
+        }
+
+        if (currentPersonAge > oldestAge) {
+            oldestAge = currentPersonAge;
+            oldestPersonObject = arrayOfPeopleObjects[i];
+        }
+    }
+
+    return oldestPersonObject;
 };
 
 // Do not edit below this line
